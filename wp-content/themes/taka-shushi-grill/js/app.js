2fd333dc289c069947cd220d9a9d62203c01e6f6
Tan Nguyen <duytan1909@gmail.com>
2016-07-09 17:49:04 +0700

    update home page layout

--- wp-content/themes/taka-shushi-grill/js/app.js
+++ wp-content/themes/taka-shushi-grill/js/app.js
@@ -9,21 +9,21 @@
         },
         
         _initFP: function () {
-            $('#wrapper').fullpage({
-                verticalCentered: false,
-                css3: true,
-                scrollingSpeed: 800,
-                // scrollBar: true
-                // ,
-                scrollOverflow: true,
-                scrollOverflowOptions: {
-                    scrollbars: true,
-                    mouseWheel: true,
-                    hideScrollbars: false,
-                    fadeScrollbars: false,
-                    disableMouse: true
-                }
-            });
+            // $('#wrapper').fullpage({
+            //     verticalCentered: false,
+            //     css3: true,
+            //     scrollingSpeed: 800,
+            //     // scrollBar: true
+            //     // ,
+            //     scrollOverflow: true,
+            //     scrollOverflowOptions: {
+            //         scrollbars: true,
+            //         mouseWheel: true,
+            //         hideScrollbars: false,
+            //         fadeScrollbars: false,
+            //         disableMouse: true
+            //     }
+            // });
         }
     };
 
--- wp-content/themes/taka-shushi-grill/parts/part-home-news.php
+++ wp-content/themes/taka-shushi-grill/parts/part-home-news.php
@@ -3,17 +3,17 @@
         <h2>bevorstehende</h2>
         <h1>VERANSTALTUNGEN</h1>
     </div>
-    <div class="event-content container-fluid">
-        <div class="col-md-5 col-md-offset-1">
+    <div class="event-content tk-container">
+        <div class="col-md-6 hidden-xs hidden-sm">
             <!--                    <img src="--><?php //echo get_bloginfo('template_url').'/img/content/home/events.jpg'; ?><!--">-->
             <div class="event-img" style="background: url('<?php echo get_bloginfo('template_url').'/img/content/home/events.jpg'; ?>')"></div>
         </div>
-        <div class="col-md-3">
+        <div class="col-xs-12 col-sm-8 col-md-4">
             <h4 class="h4">Neueröffnung</h4>
             <p>Lorem ipsum dolor sit amet,  consetetur sadipscing elitr,  sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,  sed diam voluptua.</p>
             <p class="event-time">am 01.09.2016 um 12:30 Uhr</p>
         </div>
-        <div class="col-md-2 event-date clearfix">
+        <div class="col-xs-12 col-sm-4 col-md-2 event-date clearfix">
             <div class="ed-col">
                 <div class="date-content">
                     <p class="num day">06</p>
