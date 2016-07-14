b074e1a3f5561870742ca8d776b050f08195a311
Tan Nguyen <duytan1909@gmail.com>
2016-07-14 23:28:10 +0700

    get dynamic food & drink

--- wp-content/themes/taka-shushi-grill/templates/page-menu-template.php
+++ wp-content/themes/taka-shushi-grill/templates/page-menu-template.php
@@ -7,6 +7,19 @@
  * @since Lse 1.0
  */
 get_header();
+
+function getMenu($type) {
+    $args=array(
+        'post_type' => $type,
+        'post_status' => 'publish',
+        'posts_per_page' => -1,
+        'caller_get_posts'=> 1);
+
+    return new WP_Query($args);
+}
+
+$food = getMenu("food");
+$drink = getMenu("drink");
 ?>
 <div class="block">
     <section class="section block-menu">
@@ -53,32 +66,68 @@ get_header();
         <div class="items container">
             <div class="items-food clearfix">
                 <?php
-                for($i=0;$i<8;$i++) {
-                    ?>
-                    <div class="item col-xs-12 col-sm-12 col-md-6">
-                        <a class="item-container clearfix" rel="prettyPhoto" alt="efefewf" title="YAKI-TORI<br>Description" href="<?php echo get_bloginfo('template_url').'/img/content/home/yaki.jpg'; ?>">
-                            <div class="item-thumbnail">
-                                <img class="img" src="<?php echo get_bloginfo('template_url').'/img/content/home/yaki.jpg'; ?>">
-                            </div>
-                            <div class="item-info">
-                                <div class="item-wrapper">
-                                    <div class="item-title">YAKI-TORI</div>
-                                    <div>SpeisekarteSpeisekarte Speisekarte</div>
+                if( $food->have_posts() ) {
+                    while ($food->have_posts()) : $food->the_post(); ?>
+                        <div class="item col-xs-12 col-sm-12 col-md-6">
+                            <a class="item-container clearfix" rel="prettyPhoto"
+                               href="<?php the_post_thumbnail_url("full") ?>">
+                                <div class="item-thumbnail">
+                                    <img class="img"
+                                         src="<?php the_post_thumbnail_url() ?>">
                                 </div>
-                            </div>
-                            <div class="item-price">
-                                <div>
-                                    <div class="price-label">Preis</div>
-                                    <div class="price">3.50e</div>
+                                <div class="item-info">
+                                    <div class="item-wrapper">
+                                        <div class="item-title"><?php the_title(); ?></div>
+                                        <div><?php echo get_field("item_description"); ?></div>
+                                    </div>
                                 </div>
-                            </div>
-                            <span class="hover-text">NEU</span>
-                        </a>
-                    </div>
-                <?php } ?>
+                                <div class="item-price">
+                                    <div>
+                                        <div class="price-label">Preis</div>
+                                        <div class="price"><?php echo get_field("item_price"); ?></div>
+                                    </div>
+                                </div>
+                                <span class="hover-text">NEU</span>
+                            </a>
+                        </div>
+                <?php
+                    endwhile;
+                }
+                ?>
             </div>
-            <div class="hidden items-drink clearfix"></div>
+            <div class="hidden items-drink clearfix">
+                <?php
+                if( $drink->have_posts() ) {
+                    while ($drink->have_posts()) : $drink->the_post(); ?>
+                        <div class="item col-xs-12 col-sm-12 col-md-6">
+                            <a class="item-container clearfix" rel="prettyPhoto"
+                               href="<?php the_post_thumbnail_url("full") ?>">
+                                <div class="item-thumbnail">
+                                    <img class="img"
+                                         src="<?php the_post_thumbnail_url() ?>">
+                                </div>
+                                <div class="item-info">
+                                    <div class="item-wrapper">
+                                        <div class="item-title"><?php the_title(); ?></div>
+                                        <div><?php echo get_field("item_description"); ?></div>
+                                    </div>
+                                </div>
+                                <div class="item-price">
+                                    <div>
+                                        <div class="price-label">Preis</div>
+                                        <div class="price"><?php echo get_field("item_price"); ?></div>
+                                    </div>
+                                </div>
+                                <span class="hover-text">NEU</span>
+                            </a>
+                        </div>
+                        <?php
+                    endwhile;
+                }
+                ?>
+            </div>
         </div>
     </section>
 </div>
+<?php wp_reset_query(); ?>
 <?php get_footer(); ?>
