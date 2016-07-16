19416635c2fa35411acd7604b5a7c0c7be0ab320
Tan Nguyen <duytan1909@gmail.com>
2016-07-16 12:34:31 +0700

    add news template

--- wp-content/themes/taka-shushi-grill/templates/page-news-template.php
+++ wp-content/themes/taka-shushi-grill/templates/page-news-template.php
@@ -9,12 +9,27 @@
 get_header();
 ?>
 <div class="block">
-    <section class="section bg-img" style="background: url('<?php echo get_background(); ?>')">
+    <section class="section bg-img page" style="background: url('<?php echo get_background(); ?>')">
         <div class="mask"></div>
     </section>
-    <section class="news-page">
-        <div class="container">
-
+    <section class="tk-page">
+        <div class="container news-page items-group align-row flex-wrap">
+            <?php
+                for($i=1;$i<5; $i++) {
+            ?>
+            <div class="post">
+                <a href="" class="items-group vertical-center">
+                    <div class="bg-img post-img" style="background: url('<?php echo get_bloginfo('template_url').'/img/content/home/events.jpg'; ?>')">
+                        <div class="filter"></div>
+                        <time datetime="2014-09-12">12/09</time>
+                    </div
+                    ><div class="post-content">
+                        <div class="post-title">Demter feugiata lecstcies erastoqu eagnis diste </div>
+                        <div class="post-des">antdolores rliquam eratvpatis nuytas aledatfena nece. Dabertas kyreasa nytras btreaiastas kerritas nera uisque nulinteger rutrusta.</div>
+                    </div>
+                </a>
+            </div>
+            <?php } ?>
         </div>
     </section>
 </div>
