4bec0a3098fb0d31bb3c9a5c1c721a4e584bb9c5
Duy Tan <duytan1909@gmail.com>
2016-07-20 23:52:44 +0700

    update css

--- wp-content/themes/taka-shushi-grill/parts/part-navigation.php
+++ wp-content/themes/taka-shushi-grill/parts/part-navigation.php
@@ -1,3 +1,6 @@
+<?php
+$options = get_option("eto_settings");
+?>
 <div id="logo-header" class="col-md-4 hidden-sm hidden-xs">
     <a class="logo" href="<?php echo esc_url( get_home_url() ) ?>">
         <span><img class="logo" src="<?php bloginfo('template_url') ?>/img/common/logo.jpg" alt="Logo" /></span>
@@ -10,14 +13,14 @@
         ?>
     </div>
     <div id="social" class="">
-        <span><a target="_blank" href="<?php echo get_option("eto_settings")['eto_facebook']; ?>"><i class="fa fa-facebook"></i></a></span>
+        <span><a target="_blank" href="<?php echo $options['eto_facebook'] ?>"><i class="fa fa-facebook"></i></a></span>
     </div>
 </div>
 
 <div id="mobile-nav-header" class="nav-header visible-xs-block visible-sm-block items-group align-row align-start vertical-top">
     <div class="items-group align-column">
         <div id="social">
-            <span><a target="_blank" href="<?php echo get_option("eto_settings")['eto_facebook']; ?>"><i class="fa fa-facebook"></i></a></span>
+            <span><a target="_blank" href="<?php echo $options['eto_facebook'] ?>"><i class="fa fa-facebook"></i></a></span>
         </div>
             <?php
             wp_nav_menu();
